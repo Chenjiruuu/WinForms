000c235cecc7aa339aac9d06f6c5d2dedd85f379
Chenjiruuu <152564357+Chenjiruuu@users.noreply.github.com>
2025-03-10 18:49:47 +0800

    Student_Page.cs

--- MyWinFormsApp/Student_Page.cs
+++ MyWinFormsApp/Student_Page.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -6,33 +6,57 @@ namespace WindowsFormsApp
 {
     public partial class Student_Page : Form
     {
-        // UI Components
         private PictureBox StudentPicture;
         private Button AddImageBtn, ChangeImageBtn, EditBtn;
         private Label[] Labels;
         private TextBox[] TextBoxes;
+
+        // Properties to store student data
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+        public string Year { get; set; }
+        public string Guardian { get; set; }
+        public string GuardianContact { get; set; }
+        public string Hobbies { get; set; }
+        public string Nickname { get; set; }
+
         private string[] FieldNames =
         {
             "Name", "Age", "Address", "Contact Number", "Email Address",
-            "Course and Year", "Parent/Guardian", "Parent/Guardian Contact Number",
+            "Course", "Year", "Parent/Guardian", "Parent/Guardian Contact Number",
             "Hobbies", "Nickname"
         };
 
         public Student_Page()
         {
+            StudentName = "Charles Quitaneg";
+            StudentAge = 20;
+            Address = "Warey, Malasiqui, Pangasinan";
+            ContactNumber = "09518934608";
+            Email = "quitanegcharles@gmail.com";
+            Course = "BSIT";
+            Year = "Fourth";
+            Guardian = "Ricardo Quitaneg";
+            GuardianContact = "0912345678";
+            Hobbies = "Basketball";
+            Nickname = "Chen";
+
             InitializeCustomComponents();
+            LoadStudentData(); // Load data when opening
         }
 
         private void InitializeCustomComponents()
         {
-            // Form Settings
             this.Text = "Student Page";
-            this.Size = new Size(500, 600);
+            this.Size = new Size(500, 620);
             this.StartPosition = FormStartPosition.CenterScreen;
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
 
-            // PictureBox for Student Image
             StudentPicture = new PictureBox
             {
                 Size = new Size(120, 120),
@@ -42,27 +66,24 @@ namespace WindowsFormsApp
             };
             Controls.Add(StudentPicture);
 
-            // Add Image Button
             AddImageBtn = new Button
             {
                 Text = "Add Image",
                 Location = new Point(160, 40),
                 Width = 100
             };
-            AddImageBtn.Click += AddImageBtn_Click; // Attach event
+            AddImageBtn.Click += AddImageBtn_Click;
             Controls.Add(AddImageBtn);
 
-            // Change Image Button
             ChangeImageBtn = new Button
             {
                 Text = "Change Image",
                 Location = new Point(160, 80),
                 Width = 100
             };
-            ChangeImageBtn.Click += AddImageBtn_Click; // Same function as Add Image
+            ChangeImageBtn.Click += AddImageBtn_Click;
             Controls.Add(ChangeImageBtn);
 
-            // Labels and Textboxes for Student Details
             Labels = new Label[FieldNames.Length];
             TextBoxes = new TextBox[FieldNames.Length];
 
@@ -81,33 +102,45 @@ namespace WindowsFormsApp
                 {
                     Location = new Point(180, yOffset - 3),
                     Width = 250,
-                    ReadOnly = true // Read-only unless edited
+                    ReadOnly = true
                 };
                 Controls.Add(TextBoxes[i]);
 
                 yOffset += 35;
             }
 
-            // Edit/Update Button
             EditBtn = new Button
             {
                 Text = "Edit/Update",
                 Location = new Point(180, yOffset + 10),
                 Width = 100
             };
-            EditBtn.Click += EditBtn_Click; // Attach click event
+            EditBtn.Click += EditBtn_Click;
             Controls.Add(EditBtn);
         }
 
-        // Function to open the Edit Page
         private void EditBtn_Click(object sender, EventArgs e)
         {
-            Edit_Page editForm = new Edit_Page();
+            Edit_Page editForm = new Edit_Page(this); // Pass current form
             editForm.Show();
-            this.Hide(); // Hide current form
+            this.Hide();
         }
 
-        // Function to Select and Display an Image
+        public void LoadStudentData()
+        {
+            TextBoxes[0].Text = StudentName;
+            TextBoxes[1].Text = StudentAge.ToString();
+            TextBoxes[2].Text = Address;
+            TextBoxes[3].Text = ContactNumber;
+            TextBoxes[4].Text = Email;
+            TextBoxes[5].Text = Course;
+            TextBoxes[6].Text = Year;
+            TextBoxes[7].Text = Guardian;
+            TextBoxes[8].Text = GuardianContact;
+            TextBoxes[9].Text = Hobbies;
+            TextBoxes[10].Text = Nickname;
+        }
+
         private void AddImageBtn_Click(object sender, EventArgs e)
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
